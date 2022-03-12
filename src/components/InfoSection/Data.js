fadcca6bea09d95eae29156ab24f3978405e1d94
software_rita <software.rita@correo.udistrital.edu.co>
2022-03-12 10:23:08 -0500

    secciones de informacion

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine: 'La mejor App de Finanzas',
+    headline: 'Gestiona tus ahorros, inversiones y deudas',
+    description: 'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ',
+    buttonLabel: '¡Comienza Ahora!',
+    imgStart:false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Grafico',
+    dark: true,
+    primary:true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine: 'Alcanza tus metas',
+    headline: 'Alcanza tus metas, optimizando tus ingresos',
+    description: 'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ',
+    buttonLabel: 'Más Información',
+    imgStart:true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Meta',
+    dark: false,
+    primary:false,
+    darkText: true
+};
+
+export const homeObjTree = {
+    id: 'signup',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine: 'La mejor App de Finanzas',
+    headline: 'Gestiona tus ahorros, inversiones y deudas',
+    description: 'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ',
+    buttonLabel: '¡Comienza Ahora!',
+    imgStart:false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Car',
+    dark: false,
+    primary:false,
+    darktext: true
+};
