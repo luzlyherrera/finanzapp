3ec0b164476635926542eaf4754bd92279a84adf
software_rita <software.rita@correo.udistrital.edu.co>
2022-03-11 20:27:39 -0500

    Header terminado

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import {Button} from '../ButtonElement'
+import { HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+ } from './HeroElementes'
 
 const HeroSection = () => {
-  return (
-    <HeroContainer id='home'>
+  const [hover, setHover] = useState(false)
 
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Hazlo fácil, Hazlo FinanzApp</HeroH1>
+        <HeroP>
+          Las oficinas bancarias son cosas del pasado. Registrate hoy y recibe un mes totalmente gratis. 
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button 
+            to='signup' 
+            onMouseEnter={onHover} 
+            onMouseLeave={onHover}
+            primary='true'
+            dark='true'>
+            ¡Comienza Ahora! {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
